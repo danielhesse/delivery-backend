ddb64785d6fa58a8d3b22f7ad9744c0ad0cc6d61
Daniel Hessel <hesseldaniel21@gmail.com>
2022-03-13 14:13:33 -0300

    feat: handling exceptions

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import { routes } from "./routes";
 
 const server = express();
@@ -6,6 +7,20 @@ const server = express();
 server.use(express.json());
 
 server.use(routes);
+
+server.use((err: Error, _1: Request, response: Response, _2: NextFunction) => {
+  if (err instanceof Error) {
+    const [status, message] = err.message.split('/');
+
+    return response.status(Number(status)).json({
+      message
+    });
+  }
+
+  return response.status(500).json({
+    message: `Internal server error - ${err}`
+  });
+});
 
 server.listen(3333, () => {
   console.log("🚀 Server running on port 3333!");
